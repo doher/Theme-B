66d326b8e3402562f045acbc3aee55a3bd198a5c
doher <alexandr.pletezhov@gmail.com>
2018-12-04 20:29:52 +0300

    added solution to additional task 2

---
+++ Additional-Task2/additionalTask2.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var firstLongNumber,
+    secondLongNumber,
+    firstNumbersArray = [],
+    secondNumbersArray = [],
+    resultNumbersArray = [],
+    lengthFirstArray,
+    lengthSecondArray,
+    lengthResultArray;
+
+firstLongNumber = prompt('Введите первое длинное число:');
+secondLongNumber = prompt('Введите втрое длинное число:');
+
+lengthFirstArray = firstLongNumber.length;
+lengthSecondArray = secondLongNumber.length;
+lengthResultArray = (lengthFirstArray < lengthSecondArray) ? lengthSecondArray : lengthFirstArray;
+
+firstNumbersArray = firstLongNumber.split('').reverse();
+secondNumbersArray = secondLongNumber.split('').reverse();
+
+for (let i = 0; i < lengthResultArray; i++) {
+    if (!firstNumbersArray[i]) {
+        firstNumbersArray[i] = 0;
+    }
+
+    if (!secondNumbersArray[i]) {
+        secondNumbersArray[i] = 0;
+    }
+
+    const sum = +firstNumbersArray[i] + +secondNumbersArray[i];
+
+    resultNumbersArray[i] = sum % 10;
+
+    if ((sum - (sum % 10)) == 10) {
+        if (lengthFirstArray < lengthSecondArray) {
+            secondNumbersArray[i + 1]++;
+        } else {
+            firstNumbersArray[i + 1]++;
+        }        
+    }
+}
+
+console.log('Их сумма ' + firstLongNumber + ' и ' + secondLongNumber + ' равна: ', resultNumbersArray.reverse().join(''));
