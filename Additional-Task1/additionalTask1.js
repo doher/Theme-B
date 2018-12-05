7689025c18f935a02368c3a46b8394484fd98980
doher <alexandr.pletezhov@gmail.com>
2018-12-05 14:35:47 +0300

    added new realization to task

--- Additional-Task1/additionalTask1.js
+++ Additional-Task1/additionalTask1.js
@@ -1,54 +1,29 @@
 'use strict';
 
-var n,
-    primes = [],
-    numbers = [];
-
-while (!isFinite(n) || !((n ^ 0) === n)) {
-    n = +prompt('Введите номер простого числа:');
-}
-
-var size = n;
-
-for (var i = 0; i < size; i++) {
-    numbers[i] = i;
-}
-
-primes[0] = 2;
-var i = 0;
-
-while (i < n) {
-    var prime = primes[i];
-    i++;
-
-    for (var j = prime * 2; j < size; j += prime) {
-        numbers[j] = 0;
-    }
-
-    while (numbers[prime + 1] == 0) {
-        prime++;
-    }
-
-    if ((prime + 1) >= size) {
-        var tmp = [];
-
-        for (var k = 0; k < size; k++) {
-            tmp[k] = numbers[k];
+function isPrime(num) {
+    for (let i = 2; i < num; i++) {
+        if ((num % i) == 0) {
+            return false;
         }
-
-        numbers = [];
-
-        size *= 2;
-        numbers = tmp;
-
-        for (var j = size / 2; j < size; j++) {
-            numbers[j] = j;
-        }
-
-        i = 0;
-    } else {
-        primes[i] = prime + 1;
     }
+    return true;
 }
 
-console.log(primes[n - 1]);
+var serialNumber,
+    count = 0,
+    number = 2,
+    primeNumber = number;
+
+while (!isFinite(serialNumber) || !((serialNumber ^ 0) === serialNumber) || (serialNumber <= 0)) {
+    serialNumber = +prompt('Введите номер простого числа:');
+}
+
+while (count != serialNumber) {
+    if (isPrime(number)) {
+        count++;
+        primeNumber = number;
+    }
+    number++;
+}
+
+console.log(primeNumber);
