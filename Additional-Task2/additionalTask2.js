67a9a2b08b487f0f30e23373648f414dbe4dfedc
doher <alexandr.pletezhov@gmail.com>
2018-12-05 12:35:11 +0300

    simplified script

--- Additional-Task2/additionalTask2.js
+++ Additional-Task2/additionalTask2.js
@@ -20,25 +20,21 @@ firstNumbersArray = firstLongNumber.split('').reverse();
 secondNumbersArray = secondLongNumber.split('').reverse();
 
 for (let i = 0; i < lengthResultArray; i++) {
-    if (!firstNumbersArray[i]) {
-        firstNumbersArray[i] = 0;
-    }
 
-    if (!secondNumbersArray[i]) {
-        secondNumbersArray[i] = 0;
-    }
+    firstNumbersArray[i] = firstNumbersArray[i] || 0;
+    secondNumbersArray[i] = secondNumbersArray[i] || 0;
 
     const sum = +firstNumbersArray[i] + +secondNumbersArray[i];
 
     resultNumbersArray[i] = sum % 10;
 
-    if ((sum - (sum % 10)) == 10) {
+    if (sum >= 10) {
         if (lengthFirstArray < lengthSecondArray) {
             secondNumbersArray[i + 1]++;
         } else {
             firstNumbersArray[i + 1]++;
-        }        
+        }
     }
 }
 
-console.log('Их сумма ' + firstLongNumber + ' и ' + secondLongNumber + ' равна: ', resultNumbersArray.reverse().join(''));
+console.log('Их сумма ' + firstLongNumber + ' и ' + secondLongNumber + ' равна: ' + resultNumbersArray.reverse().join(''));
