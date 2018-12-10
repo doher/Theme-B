6d9915e0d7c8abfdfd2b8bf7d87001560e4372f5
doher <alexandr.pletezhov@gmail.com>
2018-12-10 10:27:37 +0300

    remade solution

--- calculation-vowels/calculationVowels.js
+++ calculation-vowels/calculationVowels.js
@@ -1,40 +1,34 @@
 'use strict';
 
-var someWords;
+function getCountVowels(phrase) {
+    return function counter() {
+        let count = 0;
 
-while (!someWords) {
-    someWords = prompt('Введите любую строку:');
-}
-
-function getVowels(stringWords) {
-    var count = 0;
-
-    for (var i = 0; i < stringWords.length; i++) {
-        switch (stringWords.toLowerCase()[i]) {
-            case 'а':
-            case 'е':
-            case 'ё':
-            case 'о':
-            case 'и':
-            case 'э':
-            case 'у':
-            case 'ы':
-            case 'я':
-            case 'ю':
-                count++;
-                break;
+        for (let i = 0; i < phrase.length; i++) {
+            switch (phrase.toLowerCase()[i]) {
+                case 'а':
+                case 'е':
+                case 'ё':
+                case 'о':
+                case 'и':
+                case 'э':
+                case 'у':
+                case 'ы':
+                case 'я':
+                case 'ю':
+                    count++;
+                    break;
+            }
         }
-    }
 
-    if (count === 1) {
-        console.log('В вашей фразе ' + count + ' гласная буква!');
-    } else if (count > 1 && count <= 4) {
-        console.log('В вашей фразе ' + count + ' гласные буквы!');
-    } else if (count > 4) {
-        console.log('В вашей фразе ' + count + ' гласных букв!');
-    } else {
-        console.log('В вашей фразе нет гласных!');
+        return count;
     }
 }
 
-getVowels(someWords);
+var counter = getCountVowels('яблоко');
+var counter2 = getCountVowels('яблокооооооооооооооооооо');
+var counter3 = getCountVowels('блк');
+
+console.log(counter());
+console.log(counter2());
+console.log(counter3());
