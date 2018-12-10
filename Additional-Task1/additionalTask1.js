bd56ded585de397c7e3158bc13f3477f3dc90515
doher <alexandr.pletezhov@gmail.com>
2018-12-10 10:13:54 +0300

    added solution optimization

--- Additional-Task1/additionalTask1.js
+++ Additional-Task1/additionalTask1.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function isPrime(num) {
-    for (let i = 2; i < num; i++) {
+    for (let i = 2; i < (num / 2 + 1); i++) {
         if ((num % i) == 0) {
             return false;
         }
