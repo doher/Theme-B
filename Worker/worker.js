a0b5757f7de650b0c4a7d0207dd038addb72c344
doher <alexandr.pletezhov@gmail.com>
2018-12-13 15:45:14 +0300

    changed solution

--- Worker/worker.js
+++ Worker/worker.js
@@ -1,47 +1,53 @@
 'use strict';
 
 class Worker {
+
     constructor(name, surname, rate, days) {
-        let _name = name,
-            _surname = surname,
-            _rate = rate,
-            _days = days;
+        this._name = name || 'Name unknown';
+        this._surname = surname || 'Surname unknown';
+        this._rate = rate || 0;
+        this._days = days || 0;
+    }
 
-        this.getName = function () {
-            return _name || 'Name unknown';
-        }
+    get getName() {
+        return this._name;
+    }
 
-        this.getSurname = function () {
-            return _surname || 'Surname unknown';
-        }
+    get getSurname() {
+        return this._surname;
+    }
 
-        this.setRate = function (rate) {
-            _rate = rate;
-        }
+    set setRate(rate) {
+        this._rate = rate;
+    }
 
-        this.getRate = function () {
-            return _rate || 0;
-        }
+    get getRate() {
+        return this._rate;
+    }
 
-        this.setDays = function (days) {
-            _days = days;
-        }
+    set setDays(days) {
+        this._days = days;
+    }
 
-        this.getDays = function () {
-            return _days || 0;
-        }
+    get getDays() {
+        return this._days;
     }
 
     getSalary() {
-        return this.getRate() * this.getDays();
+        return this._rate * this._days;
     }
 }
 
-let worker1 = new Worker('Alex', 'Petrov', 0, 0);
-let worker2 = new Worker();
+let worker1 = new Worker('Alex', 'Petrov', 22, 43);
+let worker2 = new Worker('', 'Ivanov');
 
-worker1.setRate(80);
-worker1.setDays(21);
+worker1.setRate = 40;
+worker1.setDays = 40;
 
+worker2.setRate = 255;
+worker2.setDays = 13;
+
+console.log('Полное имя первого рабочего: ', worker1.getName + ' ' + worker1.getSurname);
+console.log('Имя второго рабочего по умолчанию: ', worker2.getName);
 console.log('Зарплата первого рабочего, к выдачи: ', worker1.getSalary());
-console.log('Имя второго рабочего по умолчанию: ', worker2.getName());
+console.log('Зарплата второго рабочего, к выдачи: ', worker2.getSalary());
